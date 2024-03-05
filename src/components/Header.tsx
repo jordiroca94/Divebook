96b4d67210ac5e3304a37a99d4b220b06f8b27da
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-03-05 20:49:19 +0100

    fix: setups

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,21 +24,28 @@ const Header = () => {
     <header
       className={`${
         scroll && "bg-black/25 text-white"
-      } px-10 py-6 fixed top-0 z-50 w-full p-6 flex justify-between items-center `}
+      } px-10 py-6 fixed top-0 z-50 w-full p-6 flex justify-between items-center border border-mediumGray  `}
     >
-      <Link href={"/"}>MY-APP</Link>
+      <Link href={"/"}>DiveBook</Link>
       <div className="flex justify-between gap-4 items-center">
         <div>One</div>
         <div>Two</div>
         <div>Three</div>
-
         <a
           className={`${
             scroll && "border-white"
-          } border-black border w-fit rounded-md py-2 px-2 cursor-pointer ml-10`}
+          } border-black border w-fit rounded-md py-2 px-4 cursor-pointer ml-10`}
           href={`${session ? "/profile" : "/login"}`}
         >
           {session ? "Profile" : "Login"}
+        </a>
+        <a
+          className={`${
+            scroll && "border-white"
+          } text-white border w-fit rounded-md py-2 px-4 cursor-pointer ml-2 bg-primary`}
+          href="/register"
+        >
+          Sign up
         </a>
       </div>
     </header>
--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import { useSession } from "next-auth/react";
-import TemporaryImage from "../../public/assets/temporary.avif";
-import Image from "next/image";
 
 const HomeHero = () => {
   const { data: session } = useSession();
@@ -10,8 +8,12 @@ const HomeHero = () => {
   return (
     <div className="flex flex-col items-center pt-28 bg-lightGray">
       {session && <div className="text-2xl ">Hello {session.user?.name}</div>}
-      <h1 className="text-4xl pb-10">WELCOME TO MY APP </h1>
-      <Image src={TemporaryImage} alt="under construction" />
+      <div className="flex items-center gap-8">
+        <h1 className="text-4xl">Dives of the World </h1>
+        <p>
+          DiveBook is a place to discover and share the best places for diving.
+        </p>
+      </div>
     </div>
   );
 };
