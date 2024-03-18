bc719e70d5758268f2a6b120c64b22ba8f0b8dd9
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-03-18 09:07:05 +0100

    feat:getDives fetch

---
+++ src/app/api/getDives/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { connectMongoDB } from "../../../../lib/mongodb";
+import Dive from "../../../../models/dive";
+
+export async function GET(req) {
+  try {
+    await connectMongoDB();
+    const dives = await Dive.find();
+    return NextResponse.json({ dives });
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/components/AllDives.tsx
+++ src/components/AllDives.tsx
@@ -1,14 +1,24 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "./ui/Container";
 
 const AllDives = () => {
+  const [data, setData] = useState([]);
+  const getAllDives = async () => {
+    const data = await fetch("api/getDives", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { dives } = await data.json();
+    setData(dives);
+  };
+
   useEffect(() => {
     getAllDives();
   }, []);
-
-  const getAllDives = async () => {};
-
+  console.log(data, "dives");
   return (
     <Container>
       <h2 className="col-span-4 lg:col-span-12 flex justify-center text-3xl lg:text-4xl text-center pb-4">
