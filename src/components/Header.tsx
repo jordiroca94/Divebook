9117ecf90b6eb403700d26aab1ac872eadd23210
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-03-24 21:13:50 +0100

    feat:favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
-
+import Icon from "../../public/assets/images/icon.png";
+import Image from "next/image";
 const Header = () => {
   const { data: session } = useSession();
 
@@ -11,8 +12,12 @@ const Header = () => {
 
   return (
     <header className="px-6 min-h-header lg:px-10 py-4 fixed top-0 z-50 w-full p-6 flex justify-between items-center border border-mediumGray bg-white">
-      <Link href={"/"} className="font-extrabold text-primary text-lg ">
-        Diverbook
+      <Link
+        href={"/"}
+        className="font-extrabold text-primary text-lg lg:text-2xl flex items-center gap-4  "
+      >
+        <span>Diverbook</span>
+        <Image className="size-10 hidden lg:block" src={Icon} alt="Home Icon" />
       </Link>
       <div className="flex justify-between gap-8 items-center">
         <div className="hidden lg:flex gap-4">
