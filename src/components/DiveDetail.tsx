32a07724964044d599e5cd98113aef5eb366b4e2
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-03-27 23:34:55 +0100

    DiveDetail

--- src/components/DiveDetail.tsx
+++ src/components/DiveDetail.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Container from "./ui/Container";
+import { DiveType } from "@/types/common";
+import Title from "./ui/Title";
+import Grid from "./ui/Grid";
 
 type Props = {
   id: string;
 };
 
 const DiveDetail = ({ id }: Props) => {
-  const [item, setItem] = useState<any>();
+  const [item, setItem] = useState<DiveType>();
   const getDives = async () => {
     try {
       const data = await fetch("/api/getDiveById", {
@@ -27,9 +30,72 @@ const DiveDetail = ({ id }: Props) => {
   useEffect(() => {
     getDives();
   }, []);
+  console.log(item, "item-->");
   return (
     <div className="pt-header">
-      <Container>{item?.name}</Container>
+      <Container>
+        {item && (
+          <>
+            <Grid>
+              <Title
+                className="col-span-4 lg:col-span-12 flex justify-center pb-10"
+                h="h1"
+              >
+                {item.name}
+              </Title>
+              <div className="lg:col-span-4 lg:col-start-2 flex flex-col justify-evenly">
+                <div className="flex items-center text-xl font-semibold gap-2 pb-6">
+                  <p>{item.location}</p>
+                  <p>-</p>
+                  <p>{item.country.label}</p>
+                </div>
+                <div>
+                  <p className="text-base uppercase py-3">Datasheet:</p>
+                  <div className="flex justify-between border-b border-mediumGray items-center gap-2 py-3">
+                    <div className="text-lg font-semibold">Instructor:</div>
+                    <p>{item.instructor}</p>
+                  </div>
+                  <div className="flex justify-between border-b border-mediumGray items-center gap-2 py-3">
+                    <div className="text-lg font-semibold">Deepth:</div>
+                    <p>{item.deepth}</p>
+                  </div>
+                  <div className="flex justify-between border-b border-mediumGray items-center gap-2 py-3">
+                    <div className="text-lg font-semibold">Suit:</div>
+                    <p>{item.suit}</p>
+                  </div>
+                  <div className="flex justify-between border-b border-mediumGray items-center gap-2 py-3">
+                    <div className="text-lg font-semibold">
+                      Water temperature:
+                    </div>
+                    <p>{item.temperature}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-span-5 lg:col-start-7">
+                <img
+                  src={item.imageUrl}
+                  alt={item.name}
+                  className="rounded-lg"
+                />
+              </div>
+            </Grid>
+            <Grid>
+              <p className="lg:col-span-7 lg:col-start-2 text-lg py-10">
+                {item?.description}
+              </p>
+              <div className="lg:col-span-3 lg:col-start-9 text-lg py-10">
+                <div className="flex gap-2 justify-end">
+                  <div className="text-lg font-semibold">Posted by:</div>
+                  <p>{item?.user.name}</p>
+                </div>
+                <div className="flex gap-2 justify-end">
+                  <p>{item?.updatedAt}</p>
+                </div>
+              </div>
+            </Grid>
+          </>
+        )}
+      </Container>
     </div>
   );
 };
