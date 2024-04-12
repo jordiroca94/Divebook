e7faa13a0d616bc93b80991199f78a19180e4adb
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-04-13 00:42:19 +0200

    feat:Burgerbutton

---
+++ src/components/BurgerButton.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { FC } from "react";
+
+type Props = {
+  open: boolean;
+  setOpen: any;
+  onClick?: () => void;
+  className?: string;
+};
+
+const BurgerButton: FC<Props> = ({
+  onClick,
+  open,
+  setOpen,
+  className = "",
+}) => {
+  const handleOpen = () => {
+    setOpen(!open);
+    if (onClick) {
+      onClick();
+    }
+  };
+
+  return (
+    <div
+      className={`${className} flex h-[22px] w-[22px] flex-col justify-center`}
+      onClick={() => handleOpen()}
+    >
+      <div
+        id="header-line-1"
+        className={`h-px w-full flex-none bg-black transition-all ease-in-out ${
+          open ? "translate-y-px rotate-[45deg] delay-300 duration-300" : ""
+        }`}
+      />
+      <div
+        id="header-line-2"
+        className={`h-px w-0 flex-none bg-black transition-all duration-300 ease-in-out ${
+          open ? "my-0 pr-0" : "my-[49%] pr-[100%] delay-300"
+        }`}
+      />
+      <div
+        id="header-line-3"
+        className={`h-px w-full flex-none bg-black transition-all ease-in-out ${
+          open ? "-translate-y-px rotate-[-45deg] delay-300 duration-300" : ""
+        }`}
+      />
+    </div>
+  );
+};
+
+export default BurgerButton;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 import Icon from "../../public/assets/images/icon.png";
 import Image from "next/image";
+import BurgerButton from "./BurgerButton";
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
+
   const { data: session } = useSession();
 
   const navLinks = [
@@ -20,12 +24,12 @@ const Header = () => {
     <header className="px-6 min-h-header lg:px-10 py-4 fixed top-0 z-50 w-full p-6 flex justify-between items-center border border-mediumGray bg-white">
       <Link
         href={"/"}
-        className="font-extrabold text-primary text-lg lg:text-2xl flex items-center gap-4  "
+        className="font-extrabold text-primary text-base lg:text-2xl flex items-center gap-4  "
       >
         <span>Diverbook</span>
         <Image className="size-10 hidden lg:block" src={Icon} alt="Home Icon" />
       </Link>
-      <div className="flex justify-between gap-8 items-center">
+      <div className="flex justify-between gap-3 lg:gap-8 items-center">
         <div className="hidden lg:flex gap-4">
           {navLinks.map((link) => (
             <Link href={link.link} key={link.label}>
@@ -36,27 +40,30 @@ const Header = () => {
         {!session ? (
           <div>
             <a
-              className="text-base font-bold text-primary  w-fit rounded-md py-2 px-4 cursor-pointer hover:text-primary/80"
+              className="text-sm lg:text-base font-semibold text-primary  w-fit rounded-md py-2 px-4 cursor-pointer hover:text-primary/80"
               href="/login"
             >
               Login
             </a>
             <a
-              className="text-base font-bold text-white border w-fit rounded-md py-2 px-4 cursor-pointer ml-2 hover:bg-primary/80 bg-primary"
+              className="text-sm lg:text-base font-semibold text-white border w-fit rounded-md py-2 px-4 cursor-pointer lg:ml-2 hover:bg-primary/80 bg-primary"
               href="/register"
             >
               Sign up
             </a>
           </div>
         ) : (
-          <Link
-            href="/profile"
-            className="flex gap-2 items-center text-primary"
-          >
+          <Link href="/profile" className="flex items-center text-primary">
             <CgProfile className="h-7 w-7" />
             <p>Profile</p>
           </Link>
         )}
+        <BurgerButton
+          open={open}
+          setOpen={setOpen}
+          onClick={() => setMobileMenu(!mobileMenu)}
+          className="lg:hidden ml-2"
+        />
       </div>
     </header>
   );
