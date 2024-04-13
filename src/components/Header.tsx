2afbaeb37a7e3ebc70ce0eecd1fde5dc86595e18
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-04-13 12:35:47 +0200

    fix: mobile navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import { CgProfile } from "react-icons/cg";
 import Icon from "../../public/assets/images/icon.png";
 import Image from "next/image";
 import BurgerButton from "./BurgerButton";
+import Button from "./ui/Button";
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
@@ -21,50 +22,76 @@ const Header = () => {
   ];
 
   return (
-    <header className="px-6 min-h-header lg:px-10 py-4 fixed top-0 z-50 w-full p-6 flex justify-between items-center border border-mediumGray bg-white">
-      <Link
-        href={"/"}
-        className="font-extrabold text-primary text-base lg:text-2xl flex items-center gap-4  "
-      >
-        <span>Diverbook</span>
-        <Image className="size-10 hidden lg:block" src={Icon} alt="Home Icon" />
-      </Link>
-      <div className="flex justify-between gap-3 lg:gap-8 items-center">
-        <div className="hidden lg:flex gap-4">
-          {navLinks.map((link) => (
-            <Link href={link.link} key={link.label}>
-              {link.label}
-            </Link>
-          ))}
-        </div>
-        {!session ? (
-          <div>
-            <a
-              className="text-sm lg:text-base font-semibold text-primary  w-fit rounded-md py-2 px-4 cursor-pointer hover:text-primary/80"
-              href="/login"
-            >
-              Login
-            </a>
-            <a
-              className="text-sm lg:text-base font-semibold text-white border w-fit rounded-md py-2 px-4 cursor-pointer lg:ml-2 hover:bg-primary/80 bg-primary"
-              href="/register"
-            >
-              Sign up
-            </a>
+    <header className="fixed top-0 z-50 w-full">
+      <div className="flex justify-between items-center px-6 min-h-header lg:px-10 py-4 p-6 border border-mediumGray bg-white">
+        <Link
+          href={"/"}
+          className="font-extrabold text-primary text-base lg:text-2xl flex items-center gap-4  "
+        >
+          <span>Diverbook</span>
+          <Image
+            className="size-10 hidden lg:block"
+            src={Icon}
+            alt="Home Icon"
+          />
+        </Link>
+        <div className="flex justify-between gap-3 lg:gap-8 items-center">
+          <div className="hidden lg:flex gap-4">
+            {navLinks.map((link) => (
+              <Link href={link.link} key={link.label}>
+                {link.label}
+              </Link>
+            ))}
           </div>
-        ) : (
-          <Link href="/profile" className="flex items-center text-primary">
-            <CgProfile className="h-7 w-7" />
-            <p>Profile</p>
-          </Link>
-        )}
-        <BurgerButton
-          open={open}
-          setOpen={setOpen}
-          onClick={() => setMobileMenu(!mobileMenu)}
-          className="lg:hidden ml-2"
-        />
+          {!session ? (
+            <div>
+              <a
+                className="text-sm lg:text-base font-semibold text-primary  w-fit rounded-md py-2 px-4 cursor-pointer hover:text-primary/80"
+                href="/login"
+              >
+                Login
+              </a>
+              <a
+                className="text-sm lg:text-base  text-white border w-fit rounded-md py-2 px-4 cursor-pointer lg:ml-2 hover:bg-primary/80 bg-primary"
+                href="/register"
+              >
+                Sign up
+              </a>
+            </div>
+          ) : (
+            <Link
+              href="/profile"
+              className="flex gap-2 items-center text-primary"
+            >
+              <CgProfile className="h-7 w-7" />
+              <p>Profile</p>
+            </Link>
+          )}
+          <BurgerButton
+            open={open}
+            setOpen={setOpen}
+            onClick={() => setMobileMenu(!mobileMenu)}
+            className="lg:hidden ml-2"
+          />
+        </div>
       </div>
+      {mobileMenu && (
+        <div className="z-40 flex w-full flex-col overflow-hidden p-6 bg-white border border-mediumGray">
+          <div className="flex flex-col items-center pt-4 gap-6">
+            {navLinks.map((link, index) => (
+              <a
+                onClick={() => {
+                  setMobileMenu(false), setOpen(!open);
+                }}
+                href={link.link}
+                key={index}
+              >
+                <h5 className="underlineAfterHover text-lg">{link.label}</h5>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </header>
   );
 };
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -20,9 +20,9 @@ const Button = ({
   secondary,
 }: Props) => {
   const styles =
-    "text-base font-bold capitalize text-white border rounded-md py-3 px-6 cursor-pointer hover:bg-primary/80 bg-primary";
+    "text-sm lg:text-base font-medium capitalize text-white border rounded-md py-3 px-6 cursor-pointer hover:bg-primary/80 bg-primary";
   const secondaryStyles =
-    "text-base capitalize lg:text-lg text-primary border-b-2 border-b-primary hover:text-primary/60 hover:border-b-primary/60";
+    "text-sm lg:text-base capitalize lg:text-lg text-primary border-b-2 border-b-primary hover:text-primary/60 hover:border-b-primary/60";
   if (link) {
     return (
       <div className={`${className}`}>
