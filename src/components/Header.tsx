1cfdfc72267cc1a609ac9ee78ff66a2a4813b13d
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-04-29 17:20:49 +0200

    feat: Animated mobileHeader

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,11 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 import BurgerButton from "./BurgerButton";
+import { motion, AnimatePresence } from "framer-motion";
+
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState<boolean>(false);
   const [open, setOpen] = useState<boolean>(false);
-
   const { data: session } = useSession();
 
   const navLinks = [
@@ -19,7 +20,7 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 z-50 w-full">
-      <div className="flex justify-between items-center px-6 min-h-header lg:px-10 py-4 p-6 border border-mediumGray bg-white">
+      <div className="relative z-40 flex justify-between items-center px-6 min-h-header lg:px-10 py-4 p-6 border border-mediumGray bg-white">
         <Link
           href={"/"}
           className="font-extrabold text-primary text-base lg:text-2xl"
@@ -66,23 +67,31 @@ const Header = () => {
           />
         </div>
       </div>
-      {mobileMenu && (
-        <div className="z-40 flex w-full flex-col overflow-hidden p-6 bg-white border border-mediumGray">
-          <div className="flex flex-col items-center pt-4 gap-6">
-            {navLinks.map((link, index) => (
-              <a
-                onClick={() => {
-                  setMobileMenu(false), setOpen(!open);
-                }}
-                href={link.link}
-                key={index}
-              >
-                <h5 className="underlineAfterHover text-lg">{link.label}</h5>
-              </a>
-            ))}
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {mobileMenu && (
+          <motion.div
+            initial={{ y: -250 }}
+            animate={{ y: 0 }}
+            exit={{ y: -250 }}
+            transition={{ ease: "easeInOut", duration: 0.5 }}
+            className="fixed pt-header top-0 left-0  z-0 flex w-full flex-col p-6 bg-white border border-mediumGray"
+          >
+            <div className="flex flex-col items-center pt-4 gap-6">
+              {navLinks.map((link, index) => (
+                <a
+                  onClick={() => {
+                    setMobileMenu(false), setOpen(!open);
+                  }}
+                  href={link.link}
+                  key={index}
+                >
+                  <h5 className="underlineAfterHover text-lg">{link.label}</h5>
+                </a>
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 };
