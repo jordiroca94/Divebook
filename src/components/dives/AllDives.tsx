a99e58639d259c0f2409f29dd4d29b64db99776d
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-05-23 18:46:13 +0200

    feat: sort button

--- src/components/dives/AllDives.tsx
+++ src/components/dives/AllDives.tsx
@@ -10,10 +10,15 @@ import BackButton from "../ui/BackButton";
 import Button from "../ui/Button";
 import DiveSkeleton from "./DiveSkeleton";
 import { formatteDate } from "@/utils/util";
+import { RiSortAsc } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa6";
 
 const AllDives = () => {
   const [data, setData] = useState<DiveType[]>([]);
   const [loadItems, setLoadItems] = useState<number>(6);
+  const [sort, setSort] = useState<"date" | "rate">("date");
+  const [openSortModal, setOpenSortModal] = useState<boolean>(false);
+
   const getAllDives = async () => {
     const data = await fetch("api/getDives", {
       method: "GET",
@@ -38,6 +43,46 @@ const AllDives = () => {
         >
           Dives of Our Users
         </Title>
+        <span className="col-span-full flex justify-end">
+          <div className="relative">
+            <button
+              onClick={() => setOpenSortModal(!openSortModal)}
+              className="flex gap-2 items-center text-base lg:text-lg hover:text-black/70"
+            >
+              <RiSortAsc className="size-6" />
+              <div>Sort by:</div>
+              <div className="capitalize">{sort}</div>
+            </button>
+            {openSortModal && (
+              <div className="border-2 bg-white border-mediumGray shadow-md rounded-md absolute top-10 right-0 ">
+                <div className="text-base lg:text-lg divide-y divide-mediumGray py-2">
+                  <button
+                    onClick={() => {
+                      setSort("date"), setOpenSortModal(false);
+                    }}
+                    className="px-4 pb-1 flex items-center gap-3 hover:text-black/70"
+                  >
+                    <div>Date</div>
+                    <FaCheck
+                      className={sort === "date" ? "visible" : "invisible"}
+                    />
+                  </button>
+                  <button
+                    onClick={() => {
+                      setSort("rate"), setOpenSortModal(false);
+                    }}
+                    className="px-4 pt-1 flex items-center gap-3 hover:text-black/70"
+                  >
+                    <div>Rate</div>
+                    <FaCheck
+                      className={sort === "rate" ? "visible" : "invisible"}
+                    />
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </span>
 
         {data.length
           ? data.slice(0, loadItems).map((item: DiveType) => {
