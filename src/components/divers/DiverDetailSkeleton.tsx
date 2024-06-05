8c39883c407168614f59e13bf9bbf6886bb11570
Jordi Roca Soler <jordirocasoler94@gmail.com>
2024-06-05 21:49:52 +0200

    fix: DiverDetailSkeleton fix

--- src/components/divers/DiverDetailSkeleton.tsx
+++ src/components/divers/DiverDetailSkeleton.tsx
@@ -6,14 +6,14 @@ const DiverDetailSkeleton = () => {
   return (
     <Grid>
       <div className="col-span-4 lg:col-span-4 lg:col-start-5 flex justify-center uppercase mb-2 lg:mb-12 h-5 bg-mediumGray "></div>
-      <div className="col-span-4 lg:col-span-4 lg:col-start-2">
-        <div className="aspect-square rounded-full bg-mediumGray animate-pulse" />
+      <div className="col-span-4 lg:col-span-5 lg:col-start-2 text-lg">
+        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
+        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
+        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
+        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
       </div>
-      <div className="col-span-4 lg:col-span-5 lg:col-start-7 text-lg">
-        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
-        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
-        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
-        <div className="w-2/5 h-5 bg-mediumGray mb-3" />
+      <div className="col-span-4 lg:col-span-4 lg:col-start-8">
+        <div className="aspect-square rounded-full bg-mediumGray animate-pulse" />
       </div>
       <div className="mb-4 col-span-4 lg:col-start-2 lg:col-span-10 bs:hidden">
         <div className="w-full h-5 bg-mediumGray mb-3" />
